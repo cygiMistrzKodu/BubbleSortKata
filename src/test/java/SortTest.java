0d616e1b33c9cd050ab6a896bc99d2c00ca0b487
Jacek programmer <jacek301@gmail.com>
2024-02-26 00:21:14 +0100

    Next Test replace 2 numbers by places

--- src/test/java/SortTest.java
+++ src/test/java/SortTest.java
@@ -13,11 +13,21 @@ public class SortTest {
         assertEquals(asList(), sort(asList()));
         assertEquals(asList(1), sort(asList(1)));
         assertEquals(asList(1, 2), sort(asList(1, 2)));
+        assertEquals(asList(1, 2), sort(asList(2, 1)));
+
 
 
     }
 
     private List<Integer> sort(List<Integer> list) {
+        if(list.size() > 1) {
+            if(list.get(0) > list.get(1)) {
+                int first = list.get(0);
+                int second = list.get(1);
+                list.set(0,second);
+                list.set(1,first);
+            }
+        }
         return list;
     }
 }
