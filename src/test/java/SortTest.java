ba4abd07206fc7905886e944585906993b97ba1a
Jacek programmer <jacek301@gmail.com>
2024-02-26 00:12:22 +0100

    starting and Degenerate test

---
+++ src/test/java/SortTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SortTest {
+
+    @Test
+    public void sorted() {
+        assertEquals(asList(), sort(asList()));
+
+
+    }
+
+    private List<Integer> sort(List<Integer> list) {
+        return new ArrayList<>();
+    }
+}
